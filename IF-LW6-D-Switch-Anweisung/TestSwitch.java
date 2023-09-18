33efcdf7b31a102980b39f0af7517a8107463cae
Legolas2222 <95238358+Legolas2222@users.noreply.github.com>
2023-09-18 12:49:02 +0200

    Worked on Dalton Assignment LW6 Switch Expression, added the IO.class library

---
+++ IF-LW6-D-Switch-Anweisung/TestSwitch.java
@@ -0,0 +1,21 @@
+public class TestSwitch {
+    public static void main(String[] args) {
+        int zahl = IO.readInt("Welchen Filmtitel möchtest du wissen?");
+        switch(zahl)
+        {
+            case 1: {
+                System.out.println("Fantastic Beasts and Where to find them"); break;
+            }
+            case 1: {
+                System.out.println("Fantastic Beasts and Where to find them"); break;
+            }
+            case 1: {
+                System.out.println("Fantastic Beasts and Where to find them"); break;
+            }
+            default: {
+                System.out.println("Ungültige Position des Lieblingsfilms!")
+            }
+        }
+        
+    }
+}   
