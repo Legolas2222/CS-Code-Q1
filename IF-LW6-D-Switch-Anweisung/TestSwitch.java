a6a3810a6a34e7caf75a370f7e547b414e1fd949
Legolas2222 <95238358+Legolas2222@users.noreply.github.com>
2023-09-22 10:17:01 +0000

    Added Agentur

--- IF-LW6-D-Switch-Anweisung/TestSwitch.java
+++ IF-LW6-D-Switch-Anweisung/TestSwitch.java
@@ -1,20 +1,22 @@
 import java.util.Scanner;
 
-public class TestSwitch{
+public class TestSwitch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Bitte gib die Nummer des nächsten Films ein");
         int zahl = scanner.nextInt();
-        switch(zahl)
-        {
+        switch (zahl) {
             case 1: {
-                System.out.println("Fantastic Beasts and Where to find them"); break;
+                System.out.println("Fantastic Beasts and Where to find them");
+                break;
             }
             case 2: {
-                System.out.println("Tenet"); break;
+                System.out.println("Tenet");
+                break;
             }
             case 3: {
-                System.out.println("Knives Out"); break;
+                System.out.println("Knives Out");
+                break;
             }
             default: {
                 System.out.println("Ungültige Position des Lieblingsfilms!");
---
+++ IF-LW7-U-Queue-Agentur/Agentur.java
@@ -0,0 +1,52 @@
+public class Agentur {
+    private Queue<Statist> statisten;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Agentur(String name) {
+        this.statisten = new Queue<Statist>();
+        this.name = name;
+    }
+
+    public Statist statistVermitteln() {
+        Statist tmp = this.statisten.front();
+        this.statisten.dequeue();
+        return tmp;
+    }
+    
+    public Statist statistVermittlen(String g) {
+        Queue<Statist> tmp = new Queue<Statist>();
+        Statist x = null;
+        while(!this.statisten.isEmpty()) {
+            if(this.statisten.front().getGeschlecht() == g) {
+                x = this.statisten.front();
+                this.statisten.dequeue();
+                
+            }
+            else {
+                tmp.enqueue(this.statisten.front());
+                this.statisten.dequeue();
+            }
+        }
+        this.statisten = tmp;
+        return x;
+    }
+
+    public void neuerStatist(Statist neuerStatist) {
+        this.statisten.enqueue(neuerStatist);
+    }
+
+    public static void main(String[] args) {
+        Agentur a = new Agentur("1");
+        a.neuerStatist(new Statist("Hans", "M"));
+        System.out.println(a.statistVermitteln().toString());
+        System.out.println("test");
+    }
+}
