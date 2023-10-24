917d0a221de9295a87eb07aca4e8667b17374930
Legolas2222 <95238358+Legolas2222@users.noreply.github.com>
2023-10-24 20:31:47 +0200

    Implemented First Prototyp of the Recursive Find inside of the Queue - has to be tested

--- IF-LW8-H-Implementation-Queue/Queue.java
+++ IF-LW8-H-Implementation-Queue/Queue.java
@@ -2,37 +2,42 @@ public class Queue<ContentType> {
     private QueueNode<ContentType> head = null;
     private QueueNode<ContentType> tail = null;
     private int length = 0;
-    
+
     public int getLength() {
         return length;
     }
+
     public Queue() {
-        
+
     }
+
     private boolean isOneElement() {
-        return (this.head == this.tail) ? true : false;
+        return (this.head == this.tail && this.length == 1) ? true : false;
     }
+
     public boolean isEmpty() {
         return (this.head == null && this.tail == null) ? true : false;
     }
+
     public ContentType getFront() {
         return (this.head != null) ? this.head.getContent() : null;
     }
-    public void enqueue(ContentType t) {
+
+    public ContentType enqueue(ContentType t) {
         QueueNode<ContentType> newNode = new QueueNode<ContentType>(t);
         if (this.isEmpty()) {
             this.head = newNode;
             this.tail = newNode;
             this.length++;
-            return;
-        }
-        else {
+            return newNode.contentObj;
+        } else {
             this.tail.setNext(newNode);
             this.tail = newNode;
             this.length++;
-            return;
+            return newNode.contentObj;
         }
     }
+
     public void dequeue() {
         if (this.isEmpty()) {
             return;
@@ -42,44 +47,70 @@ public class Queue<ContentType> {
             this.tail = null;
             this.length = 0;
             return;
-        }
-        else {
+        } else {
             QueueNode<ContentType> secondNode = this.head.getNext();
             this.head = secondNode;
             this.length--;
             return;
         }
     }
-    
-    // public static void main(String[] args) {
-    //     // Arrange
-    //     Queue<Person> q = new Queue<Person>();
-    //     Person p = new Person("Bob");
-    //     // Act
-    //     q.enqueue(p);
-    //     // Assert
-    //     if (p.equals(q.getFront())) {
-    //         System.out.println("Gleiche Elemente");
-    //     }
-    //     // Act
-    //     q.dequeue();
-    //     // Assert
-    //     if (q.isEmpty()) {
-    //         System.out.println("Ist leer");
-    //     }
-    // }
+
+    public ContentType findElement(ContentType r) {
+        if (this.isEmpty()) {
+            return null;
+        }
+        if (this.isOneElement()) {
+            return (r == this.getFront()) ? this.getFront() : null;
+        }
+        return recursiveFind(this.head, r, 0);
+
+    }
+
+    private ContentType recursiveFind(QueueNode<ContentType> previousNode, ContentType refObject, int currentCounter) {
+        if (previousNode == null || currentCounter == this.length) {
+            return null;
+        }
+        currentCounter++;
+        if (previousNode.contentObj != refObject) {
+            recursiveFind(previousNode.getNext(), refObject, currentCounter);
+        }
+        return previousNode.getContent();
+    }
+
+    public static void main(String[] args) {
+        // Arrange
+        Queue<Person> q = new Queue<Person>();
+        Person p1 = new Person("Bob");
+        Person p2 = new Person("Max");
+
+        // Act
+        q.enqueue(p1);
+        q.enqueue(p2);
+        if (q.findElement(p2) == p2) {
+            System.out.println("gefunden");
+        }
+        
+        //Person result = find(p2);
+        // Assert
+
+    }
+
     public class QueueNode<SubContentType> {
         private SubContentType contentObj;
         private QueueNode<SubContentType> nextNode = null;
+
         public QueueNode(SubContentType t) {
             this.contentObj = t;
         }
+
         public QueueNode<SubContentType> getNext() {
             return this.nextNode;
         }
+
         public void setNext(QueueNode<SubContentType> qn) {
             this.nextNode = qn;
         }
+
         public SubContentType getContent() {
             return this.contentObj;
         }
